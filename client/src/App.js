0d1c648ef657ba474aef6de22ba3eb2f3ee5a68a
Daniel Luna <daniel.luna@correounivalle.edu.co>
2020-12-16 17:54:34 -0500

    CRUD finished

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,11 @@ function App() {
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
 
-  const addEmployee = () => {
+  const [newWage, setNewWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const addEmployee = () =>{
     Axios.post('http://localhost:3001/create', {
       name: name,
       age: age,
@@ -18,13 +22,57 @@ function App() {
       position: position,
       wage: wage,
     }).then(()=>{
-      console.log("Success");
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
     })
+  };
+
+  const getEmployees = () => {
+    Axios.get('http://localhost:3001/employees').then((response)=>{
+      setEmployeeList(response.data);
+    });
+  };
+
+  const updateEmployeeWage = (id) => {
+    Axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
+      (response) => {
+        setEmployeeList(
+          employeeList.map((val) => {
+            return val.id === id
+              ? {
+                  id: val.id,
+                  name: val.name,
+                  country: val.country,
+                  age: val.age,
+                  position: val.position,
+                  wage: newWage,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response)=>{
+      setEmployeeList(employeeList.filter((val)=> {
+        return val.id !== id;
+      }));
+    });
   };
 
   return (
     <div className="App">
-      <div class="information">
+      <div className="information">
         <label>Name:</label>
         <input type="text" onChange={(event) => {
             setName(event.target.value);
@@ -46,6 +94,32 @@ function App() {
             setWage(event.target.value);
           }}/>
         <button onClick={addEmployee}>Add Employee</button>
+      </div>
+    {/* Aqui empieza la linea */}
+      <div className="employees">
+        <button onClick={getEmployees}>Show Employees</button>
+
+        {employeeList.map((val, key) => {
+          return(
+            <div className="employee">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Country: {val.country}</h3>
+                <h3>Position: {val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+              <div>
+                <input type="text" placeholder="2000..." onChange={(event) => 
+                  {
+                    setNewWage(event.target.value);
+                  }}/>
+                <button onClick={()=>{updateEmployeeWage(val.id)}}>Update</button>
+                <button onClick={()=>{deleteEmployee(val.id)}}>Delete</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -31,6 +31,39 @@ app.post('/create', (req, res) => {
 
 });
 
+app.get('/employees', (req, res) => {
+    db.query("Select * from employees", (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query("UPDATE employees SET wage = ? WHERE id = ?", [wage, id], (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM employees WHERE id = ?", id, (err, result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    });
+})
+
 app.listen(3001, ()=>{
     console.log("Server running");
 })
